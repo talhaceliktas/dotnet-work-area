40a3bd6e0a817659217f7d403ff1bb185bf59d5c
talhaceliktas <avrol928@gmail.com>
2026-03-24 00:35:03 +0300

    feat: implement calculator API logic
    Replaces the placeholder "Hello World" endpoint with a `/calculate` endpoint that performs basic arithmetic operations based on query parameters and returns a JSON response.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,6 +1,58 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (HttpContext context) =>
+{
+    context.Response.Redirect("http://localhost:5248/calculate?firstNumber=123&secondNumber=456&operation=multiply");
+});
+
+app.MapGet("/calculate", (HttpContext context) =>
+{
+    context.Response.ContentType = "application/json";
+
+    IQueryCollection queries = context.Request.Query;
+
+    var isError = !queries.ContainsKey("firstNumber") || !queries.ContainsKey("secondNumber")
+    || !queries.ContainsKey("operation");
+
+    object jsonObject;
+
+    if (!isError)
+    {
+        double firstNumber = Convert.ToDouble(queries["firstNumber"][0]);
+        double secondNumber = Convert.ToDouble(queries["secondNumber"][0]);
+        string operation = queries["operation"][0] ?? "plus";
+        double result;
+
+        if (operation == "plus")
+            result = firstNumber + secondNumber;
+        else if (operation == "minus")
+            result = firstNumber - secondNumber;
+        else if (operation == "divide")
+            result = firstNumber / secondNumber;
+        else if (operation == "multiply")
+            result = firstNumber * secondNumber;
+        else
+            result = 0;
+
+        jsonObject = new
+        {
+            status = "Succes",
+            firstNumber,
+            secondNumber,
+            operation,
+            result,
+        };
+    }
+    else
+    {
+        jsonObject = new
+        {
+            status = "Error"
+        };
+    }
+
+    context.Response.WriteAsJsonAsync(jsonObject);
+});
 
 app.Run();

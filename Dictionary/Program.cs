8d5b951c3003fe96a70c3e4194eee45c054a6910
talhaceliktas <avrol928@gmail.com>
2026-03-24 00:28:22 +0300

    feat: implement request body parsing and data processing
    Update Program.cs to read the request body as a dictionary, enabling the application to extract specific fields and calculate total prices from the input data.

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) => {
+    StreamReader sr = new StreamReader(context.Request.Body);
+    string body = await sr.ReadToEndAsync();
+
+    Dictionary<string, StringValues> dictionary = QueryHelpers.ParseQuery(body);
+
+    if (dictionary.ContainsKey("firstName"))
+    {
+        string firstName = dictionary["firstName"][0] ?? "";
+        await context.Response.WriteAsync($"Isim: {firstName}");
+
+    }
+
+    int price = 0;
+    if (dictionary.ContainsKey("price"))
+    {
+
+        foreach (string? sayi in dictionary["price"])
+        {
+            if (string.IsNullOrEmpty(sayi)) continue;
+            price += Convert.ToInt32(sayi);
+        }
+    }
+
+    await context.Response.WriteAsync($"Fiyatlar: {price}");
+});
+
 
 app.Run();

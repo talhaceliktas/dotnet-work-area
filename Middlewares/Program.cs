f75d49effd08143b5d4ca0f0d881372967f78c7e
talhaceliktas <avrol928@gmail.com>
2026-03-24 00:40:42 +0300

    feat: implement error handling middleware
    Add a custom middleware component to catch and log exceptions globally. This update includes a test route that triggers an exception to demonstrate the error-handling behavior and updates the root endpoint response.

--- Middlewares/Program.cs
+++ Middlewares/Program.cs
@@ -1,6 +1,38 @@
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Kronometre
+//app.UseMiddleware<MyCustomMiddleware>();
+
+// Middleware 2
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    try
+    {
+        await next(context);
+    }
+    catch (Exception ex)
+    {
+        await context.Response.WriteAsync("Status:Error");
+
+        Console.WriteLine(ex.ToString());
+
+        return;
+    }
+
+});
+
+
+app.MapGet("/hata", () =>
+{
+    throw new NotImplementedException();
+});
+
+app.MapGet("/", async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("Status: Success");
+
+});
 
 app.Run();

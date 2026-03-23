bf966aa9063d7f2e7afefe6ac8d99caca16a3b52
talhaceliktas <avrol928@gmail.com>
2026-03-24 01:40:53 +0300

    refactor: move payment security logic to standalone IMiddleware class
    Extracts the inline validation logic from Program.cs into a dedicated OdemeFedaisi class and adds an extension method for cleaner registration. This refactoring improves code organization by separating the security concerns from the main application configuration and utilizes the dependency injection container for middleware registration.

---
+++ Example-1/OdemeFedaisi.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Example_1
+{
+    public class OdemeFedaisi : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+                if (context.Request.Method != "POST")
+                {
+                    context.Response.StatusCode = 405;
+                    return;
+                }
+
+                if (!context.Request.Headers.ContainsKey("X-Firma-Token"))
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+
+                if (context.Request.Headers["X-Firma-Token"][0] != "MultiPayVIP")
+                {
+                    context.Response.StatusCode = 401;
+                    return;
+                }
+
+                context.Response.Headers["X-Guvenlik-Kontrolu"] = "Gecti";
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync("Success");
+                await next(context);
+        }
+
+    }
+    
+    public static class OdemeFedaisiExtension
+    {
+        public static IApplicationBuilder UseOdemeFedaisi(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<OdemeFedaisi>();
+        }
+    }
+}
--- Example-1/Program.cs
+++ Example-1/Program.cs
@@ -1,34 +1,15 @@
+using Example_1;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<OdemeFedaisi>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
 app.UseWhen(context => context.Request.Path.StartsWithSegments("/api/odeme"), (IApplicationBuilder app) =>
 {
-    app.Use(async (HttpContext context, RequestDelegate next) => {
-        if(context.Request.Method != "POST")
-        {
-            context.Response.StatusCode = 405;
-            return;
-        }
-
-        if (!context.Request.Headers.ContainsKey("X-Firma-Token"))
-        {
-            context.Response.StatusCode = 401;
-            return;
-        }
-
-        if (context.Request.Headers["X-Firma-Token"][0] != "MultiPayVIP")
-        {
-            context.Response.StatusCode = 401;
-            return;
-        }
-
-        context.Response.Headers["X-Guvenlik-Kontrolu"] = "Gecti";
-
-        await next(context);
-
-    });
+    app.UseOdemeFedaisi();
 });
 
 app.Run();

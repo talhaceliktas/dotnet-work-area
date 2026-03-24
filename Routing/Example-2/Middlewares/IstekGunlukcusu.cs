d06845c845a9fce0b60b8b71fa902813213eba52
talhaceliktas <avrol928@gmail.com>
2026-03-24 22:17:39 +0300

    feat: add request logging middleware to Example-2
    Introduces the IstekGunlukcusu middleware skeleton and its corresponding extension method to provide a base for request logging as part of the middleware pipeline exercises.

---
+++ Routing/Example-2/Middlewares/IstekGunlukcusu.cs
@@ -0,0 +1,23 @@
+﻿namespace Example_2.Middlewares
+{
+    public class IstekGunlukcusu
+    {
+        private readonly RequestDelegate _next;
+
+        public IstekGunlukcusu(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke() {
+        
+        }
+    }
+
+    public static class IstekGunlukcusuExtensions
+    {
+        public static IApplicationBuilder UseIstekGunlukcusu(this IApplicationBuilder builder) {
+            return builder.UseMiddleware<IstekGunlukcusu>();
+        }
+    }
+}

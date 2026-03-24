7558c28c0ffa2af6ea3eb306d8838f0008e0afdb
talhaceliktas <avrol928@gmail.com>
2026-03-24 22:24:22 +0300

    feat: add authorization middleware to Example-2
    Introduces the YetkiKontrolcusu middleware skeleton to demonstrate header-based permission control by inspecting the X-API-KEY header as part of the middleware pipeline exercises.

---
+++ Routing/Example-2/Middlewares/YetkiKontrolcusu.cs
@@ -0,0 +1,14 @@
+﻿namespace Example_2.Middlewares
+{
+    public class YetkiKontrolcusu : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next) {
+            if(context.Request.Headers.ContainsKey("X-API-KEY")) {
+                context.Response.StatusCode = 401;
+                return;
+            }
+
+            await next(context);
+        }
+    }
+}

babfdd607d54412efd4540215b835cb0d509c8e5
talhaceliktas <avrol928@gmail.com>
2026-03-25 20:38:42 +0300

    feat: add attribute routing to HomeController
    Applies the [Route] attribute to map the method1 action to the "sayhello" endpoint, enabling explicit URL mapping for the controller.

--- ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace ControllersExample.Controllers
+namespace ControllersExample.Controllers 
 {
     public class HomeController
     {
+        [Route("sayhello")]
         public string method1() {
             return "hello from method1";
         }

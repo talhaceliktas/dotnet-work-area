28e052b99ae1f7cb8d8fb37506f1f042fe922ee8
talhaceliktas <avrol928@gmail.com>
2026-03-25 13:08:09 +0300

    feat: implement global error handling middleware in Example-2
    Adds a centralized middleware to catch unhandled exceptions and return a standardized JSON error response with a 500 status code. It includes an extension method to simplify registration within the request pipeline.

---
+++ Routing/Example-2/Middlewares/GlobalErrorHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace Example_2.Middlewares
+{
+    public class GlobalErrorHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) {
+                if (context.Response.HasStarted)
+                {
+                    Console.WriteLine("Hata olustu: " + ex.ToString());
+                    return;
+                }
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    hata = "Sunucu patladı",
+                    zaman = DateTimeOffset.Now.ToUnixTimeSeconds()
+
+                });
+
+
+            
+            
+            }
+
+        }
+    }
+
+    public static class GlobalErrorHandlerExtensions
+    {
+        public static IApplicationBuilder UseGlobalErrorHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GlobalErrorHandler>();
+        }
+    }
+}

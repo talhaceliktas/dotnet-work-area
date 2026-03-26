99e942a586e8ddbe84fb37bd00fe16835f47c31a
talhaceliktas <avrol928@gmail.com>
2026-03-26 16:47:34 +0300

    feat: demonstrate diverse IActionResult responses and reorganize project layout
    Adds KitapController to showcase handling various HTTP status codes such as 400, 401, 403, and 404 for input validation and simulated authorization. This update also moves the Example-3 project into a categorized directory structure for better organization.

---
+++ Controllers/Example-3/Controllers/KitapController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example_3.Controllers
+{
+    public class KitapController : Controller
+    {
+        [HttpGet("kitap")]
+        public IActionResult Index(int? kitapId, bool? girisYapildi)
+        {
+
+            if (!Request.Query.ContainsKey("kitapId"))
+            {
+                return BadRequest("KitapId Zorunludur.");
+            }
+
+            if (kitapId <= 0)
+            {
+                return BadRequest("KitapId 0 dan buyuk olmali.");    
+            }
+
+            if (kitapId > 1000)
+            {
+                return NotFound("Bu Id'de kitap yok.");
+            }
+
+            if (!girisYapildi.HasValue) {
+                return Unauthorized();
+            }
+
+            if (!girisYapildi ?? false)
+            {
+                return StatusCode(403, "Giriş yapılmış ama yetkin yok");
+            }
+
+            return Content("Kitap bilgileri ID: " + kitapId, "text/plain");
+
+        }
+    }
+}

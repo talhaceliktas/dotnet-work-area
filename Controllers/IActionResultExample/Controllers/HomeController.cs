d55a494a8d392d65227ef20c977f95c72bfa8156
talhaceliktas <avrol928@gmail.com>
2026-03-26 12:27:20 +0300

    feat: implement HomeController and enable controller-based routing
    Replaces the minimal API setup in Program.cs with controller-based routing. Adds a HomeController featuring an Index action that demonstrates query string validation and the use of ContentResult to return plain-text responses based on input logic.

---
+++ Controllers/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ContentResult Index()
+        {
+            // bookId should be supllied
+            if (!Request.Query.ContainsKey("bookId"))
+            {
+                return Content("bookId is not supplied");
+            }
+
+            // bookId can't be empty
+            if (string.IsNullOrWhiteSpace(Request.Query["bookId"][0]))
+            {
+                return Content("bookId can't be null or whitespace");
+            }
+
+            // bookId should be between 1 and 1000
+            int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookId"]);
+
+            if (bookId <= 0) {
+                return Content("Book id can't be less then or equal zero.");
+            }
+
+            if (bookId > 1000)
+            {
+                return Content("Book id can't be greater than 1000");
+            }
+
+        }
+    }
+}

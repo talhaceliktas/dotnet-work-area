afe1c6bf70ad144361686d9b26d01a79a327a1cb
talhaceliktas <avrol928@gmail.com>
2026-03-27 20:03:58 +0300

    feat: add ModelValidationExample project and initial Person model
    Initializes a new project for demonstrating model validation, including a Person model and a HomeController with a POST action to handle registration data.

---
+++ Model Binding & Validations/ModelValidationExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationExample.Controllers;
+using ModelValidationExample.Models;
+namespace ModelValidationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost("register")]
+        public IActionResult Index(Person person)
+        {
+            return Content(person.ToString());
+        }
+    }
+}
---
+++ Model Binding & Validations/ModelValidationExample/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace ModelValidationExample.Models
+{
+    public class Person
+    {
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person Object - Person Name: {PersonName}" +
+                   $"Email {Email}" +
+                   $"Email {Phone}" +
+                   $"Email {Password}" +
+                   $"Email {ConfirmPassword}" +
+                   $"Email {Price}";
+
+
+        }
+
+    }
+}

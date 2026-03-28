cfdbce271d6adbdaa903c3878b63ff4c2585260a
talhaceliktas <avrol928@gmail.com>
2026-03-28 16:52:14 +0300

    feat: add [RegularExpression] to PersonName and [Range] validation to Price
    Applies character restrictions to the PersonName property and enforces a valid numeric range for the Price property. Includes custom error messages for both attributes to improve validation feedback.

--- Model Binding & Validations/ModelValidationExample/Models/Person.cs
+++ Model Binding & Validations/ModelValidationExample/Models/Person.cs
@@ -8,12 +8,15 @@ namespace ModelValidationExample.Models
         [Required(ErrorMessage = "{0} can't be null or empty.")]
         [DisplayName("Personel Name")]
         [StringLength(40, MinimumLength = 5, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
+        [RegularExpression(@"^[\w. ]+$", ErrorMessage = "{0} should contain only alphabets, space and dot (.)")]
         public string? PersonName { get; set; }
 
         public string? Email { get; set; }
         public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
+
+        [Range(0, 999.99, ErrorMessage = "{0} should be between {1} and {2}")]
         public double? Price { get; set; }
 
         public override string ToString()

f1a211dfef4e248f3f55093a404171a826459a04
talhaceliktas <avrol928@gmail.com>
2026-03-30 13:35:08 +0300

    feat: add RegisterRequest model with data annotation validations
    Introduces a new request model for the registration process, utilizing various built-in attributes to demonstrate field validation, including pattern matching, range checks, and property comparisons.

---
+++ Model Binding & Validations/Example-4/Models/RegisterRequest.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Example_4.Models
+{
+    public record RegisterRequest
+    {
+        [Required(ErrorMessage = "{0} girmeniz zorunludur!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Lutfen {1} ve {2} uzunluklari arasinda metin girin!")]
+        [RegularExpression(@"[a-zA-Z0-9_]")]
+        string? Username { get; init; }
+
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Lutfen gecerli bir mail adresi giriniz!")]
+        string? Email { get; init; }
+
+        [Phone(ErrorMessage = "Lutfen gecerli bir telefon giriniz!")]
+        string? Phone { get; init; }
+
+        [Required(ErrorMessage = "Sifre alani bos olamaz!")]
+        string? Password { get; init; }
+
+        [Required(ErrorMessage = "Sifre alani bos olamaz!")]
+        [Compare(nameof(Password), ErrorMessage = "Lutfen iki sifreyi ayni giriniz")]
+        string? PasswordConfirm { get; init; }
+
+        [Range(18, 99, ErrorMessage = "Lutfen {1} - {2} araliginda yas giriniz!")]
+        int? Age { get; init; }
+
+        [Url(ErrorMessage = "Lutfen gecerli bir website giriniz!")]
+        string? Website {  get; init; }
+
+    }
+}

bd52e57a589a0d78fd1efb202f8c4c4ea1a32292
talhaceliktas <avrol928@gmail.com>
2026-03-30 13:58:44 +0300

    feat: implement RegisterController and update RegisterRequest model
    Adds the RegisterController to handle registration requests using body binding. Updates the RegisterRequest model by making properties public to enable successful model binding and refining the username validation regex.

--- Model Binding & Validations/Example-4/Models/RegisterRequest.cs
+++ Model Binding & Validations/Example-4/Models/RegisterRequest.cs
@@ -6,29 +6,29 @@ namespace Example_4.Models
     {
         [Required(ErrorMessage = "{0} girmeniz zorunludur!")]
         [StringLength(20, MinimumLength = 3, ErrorMessage = "Lutfen {1} ve {2} uzunluklari arasinda metin girin!")]
-        [RegularExpression(@"[a-zA-Z0-9_]")]
-        string? Username { get; init; }
+        [RegularExpression(@"^[a-zA-Z0-9_]+$")]
+        public string? Username { get; init; }
 
 
         [Required]
         [EmailAddress(ErrorMessage = "Lutfen gecerli bir mail adresi giriniz!")]
-        string? Email { get; init; }
+        public string? Email { get; init; }
 
         [Phone(ErrorMessage = "Lutfen gecerli bir telefon giriniz!")]
-        string? Phone { get; init; }
+        public string? Phone { get; init; }
 
         [Required(ErrorMessage = "Sifre alani bos olamaz!")]
-        string? Password { get; init; }
+        public string? Password { get; init; }
 
         [Required(ErrorMessage = "Sifre alani bos olamaz!")]
         [Compare(nameof(Password), ErrorMessage = "Lutfen iki sifreyi ayni giriniz")]
-        string? PasswordConfirm { get; init; }
+        public string? PasswordConfirm { get; init; }
 
         [Range(18, 99, ErrorMessage = "Lutfen {1} - {2} araliginda yas giriniz!")]
-        int? Age { get; init; }
+        public int? Age { get; init; }
 
         [Url(ErrorMessage = "Lutfen gecerli bir website giriniz!")]
-        string? Website {  get; init; }
+        public string? Website {  get; init; }
 
     }
 }

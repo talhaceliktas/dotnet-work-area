ec0282e150a637f52eb1e0ffa140f76e63a616bd
talhaceliktas <avrol928@gmail.com>
2026-03-30 14:38:46 +0300

    feat: implement EventController and EventRequest model
    Adds a new controller and request model to demonstrate the application of the custom FutureDate validation attribute. The endpoint accepts an event request and validates that the provided start date is in the future.

---
+++ Model Binding & Validations/Example-4/Controllers/EventController.cs
@@ -0,0 +1,16 @@
+﻿using Example_4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example_4.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class EventController : ControllerBase
+    {
+        [HttpPost("event")]
+        public IActionResult Index([FromBody] EventRequest request)
+        {
+            return Ok(new {Success= true, request});
+        }
+    }
+}
---
+++ Model Binding & Validations/Example-4/Models/EventRequest.cs
@@ -0,0 +1,12 @@
+﻿using Example_4.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Example_4.Models
+{
+    public record EventRequest
+    {
+        [Required(ErrorMessage = "{0} alani bos birakilamaz!")]
+        [FutureDate]
+        public DateTime? StartDate { get; init; }
+    }
+}

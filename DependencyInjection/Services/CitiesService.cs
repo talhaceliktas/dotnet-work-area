3d6012b62de4bdf31a611579c804fce953e5dc49
talhaceliktas <avrol928@gmail.com>
2026-03-30 17:04:50 +0300

    feat: initialize dependency injection example and service projects
    Creates a new ASP.NET Core web application and a services class library. This provides the project structure and a basic `CitiesService` to demonstrate dependency injection.

---
+++ DependencyInjection/Services/CitiesService.cs
@@ -0,0 +1,25 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "London",
+                "Berlin",
+                "Mardin",
+                "Lisbon",
+                "Istanbul",
+                "Tokyo"
+            };   
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}

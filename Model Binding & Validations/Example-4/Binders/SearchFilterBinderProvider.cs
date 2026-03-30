4f6dd647c716747d2aea8db77060c82bab73465f
talhaceliktas <avrol928@gmail.com>
2026-03-30 15:35:24 +0300

    feat: implement SearchFilterBinderProvider
    Adds a model binder provider to register the custom SearchFilterBinder for the SearchFilter type. This enables the framework to automatically resolve and apply the custom binding logic whenever the SearchFilter model is used in a controller action.

---
+++ Model Binding & Validations/Example-4/Binders/SearchFilterBinderProvider.cs
@@ -0,0 +1,17 @@
+﻿using Example_4.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Example_4.Binders
+{
+    public class SearchFilterBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType == typeof(SearchFilter))
+                return new BinderTypeModelBinder(typeof(SearchFilterBinder));
+
+            return null;
+        }
+    }
+}

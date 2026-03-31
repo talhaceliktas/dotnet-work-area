e4e76df29557fd3486fc2dd0afee9708f8f5ac0b
talhaceliktas <avrol928@gmail.com>
2026-03-31 20:21:44 +0300

    feat: introduce ICitiesService and decouple HomeController
    Creates a ServiceContracts project and defines the ICitiesService interface. The HomeController is updated to depend on the interface instead of the concrete CitiesService class, preparing the project for dependency injection.

--- DependencyInjection/DIExample/Controllers/HomeController.cs
+++ DependencyInjection/DIExample/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
 using Services;
 
 namespace DIExample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly CitiesService _citiesService;
+        private readonly ICitiesService _citiesService;
 
         public HomeController()
         {
-            _citiesService = new CitiesService();
+            _citiesService = null;
         }
 
         [Route("/")]

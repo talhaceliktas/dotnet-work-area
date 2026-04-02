1b7a0a2680e4ec41c77124c5e229cc67a88aa7ea
talhaceliktas <avrol928@gmail.com>
2026-04-02 14:39:31 +0300

    feat: implement weather service and caching decorator for Task-4
    Adds RealWeatherService to simulate weather data retrieval and CachingWeatherServiceDecorator to implement a caching layer using IMemoryCache. These implementations provide the core logic for demonstrating the decorator pattern within the Task-4 example.

---
+++ DependencyInjection/Examples/Task-4/RealWeatherService/Decorators/CachingWeatherServiceDecorator.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Microsoft.Extensions.Caching.Memory;
+using ServiceContracts;
+namespace Services.Decorators
+{
+    public class CachingWeatherServiceDecorator
+    {
+        private readonly IWeatherService _inner;
+        private readonly IMemoryCache _cache;
+
+        public CachingWeatherServiceDecorator(IWeatherService inner, IMemoryCache cache) {
+            _inner = inner;
+            _cache = cache;
+        }
+
+        public async Task<WeatherReport> GetCurrentWeatherAsync(string city)
+        {
+            string cacheKey = $"weather_{city.ToLower()}";
+
+            var weatherReport = await _cache.GetOrCreateAsync(cacheKey, async entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+
+                Console.WriteLine($"[CACHE MISS] {city} için veri önbellekte yok, API'ye gidiliyor...");
+
+                return await _inner.GetCurrentWeatherAsync(city);
+            });
+
+            return weatherReport ?? throw new InvalidOperationException("Hava durumu verisi çekilemedi!");
+        }
+    }
+}
---
+++ DependencyInjection/Examples/Task-4/RealWeatherService/RealWeatherService.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using ServiceContracts;
+
+namespace Services
+{
+    public class RealWeatherService : IWeatherService
+    {
+        public async Task<WeatherReport> GetCurrentWeatherAsync(string city)
+        {
+            await Task.Delay(2000);
+
+            Random random = new Random();
+            double randomNumber = random.NextDouble() * 35;
+
+            WeatherReport weatherReport = new WeatherReport
+            {
+                City = city,
+                TemperatureCelsius = Math.Round(randomNumber, 1),
+                Description = "Parçalı Bulutlu", 
+                FetchedAt = DateTime.Now 
+            };
+
+            return weatherReport;
+            
+        }
+    }
+}

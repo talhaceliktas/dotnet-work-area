38f18c6000e651f75f2b5e70386c4b2ec6728e20
talhaceliktas <avrol928@gmail.com>
2026-04-02 16:00:28 +0300

    feat: display current environment on home page
    Injects IWebHostEnvironment into the HomeController to retrieve and display the active environment name on the Index view. Also comments out the exception test route to clean up the UI.

--- EnvironmentsExample/Controllers/HomeController.cs
+++ EnvironmentsExample/Controllers/HomeController.cs
@@ -4,16 +4,24 @@ namespace EnvironmentsExample.Controllers
 {
     public class HomeController : Controller
     {
+        private IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [HttpGet("/")]
         public IActionResult Index()
         {
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
             return View();
         }
 
-        [HttpGet("/some-route")]
-        public IActionResult Other()
-        {
-            throw new NotImplementedException();
-        }
+        //[HttpGet("/some-route")]
+        //public IActionResult Other()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }

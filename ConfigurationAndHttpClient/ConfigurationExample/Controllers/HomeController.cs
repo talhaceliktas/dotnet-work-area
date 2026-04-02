8fb1f461985d021869ab8ff81786cbe3e0d98fe8
talhaceliktas <avrol928@gmail.com>
2026-04-02 18:05:35 +0300

    feat: display configuration values in home view using dependency injection
    Updates the HomeController to inject IConfiguration and retrieves specific keys with default fallbacks, passing them to the Index view via ViewBag for display.

--- ConfigurationAndHttpClient/ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationAndHttpClient/ConfigurationExample/Controllers/HomeController.cs
@@ -4,9 +4,19 @@ namespace ConfigurationExample.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration) {
+            _configuration = configuration;
+        }
+
+
         [HttpGet("/")]
         public IActionResult Index()
         {
+            ViewBag.MyKey = _configuration.GetValue<string>("MyKey", "Test");
+            ViewBag.MyAPIKey = _configuration.GetValue<string>("MyApiKey", "AAADDSSSS22222");
+
             return View();
         }
     }

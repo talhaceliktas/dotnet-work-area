53cf8e7e00e9a078d0d3d2c0ac3bdd1064d7e62d
talhaceliktas <avrol928@gmail.com>
2026-04-02 14:52:41 +0300

    feat: add HomeController and configure project references for Task-4
    Adds the HomeController to provide a web interface for retrieving weather data and updates project references to link the service contracts, entities, and implementations. These changes integrate the various projects within the Task-4 example into a functional web application.

---
+++ DependencyInjection/Examples/Task-4/WeatherWithCaching/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace WeatherWithCaching.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWeatherService _weatherService; 
+
+        public HomeController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet("weather/{city}")]
+        public async Task<IActionResult> Index([FromRoute] string city)
+        {
+            var weatherData = await _weatherService.GetCurrentWeatherAsync(city);
+
+            return Ok(weatherData);
+        }
+    }
+}

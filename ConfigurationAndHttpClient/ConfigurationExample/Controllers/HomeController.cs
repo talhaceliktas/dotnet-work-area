14f2c42d9ceab18e3a5a9c4902149e92fbfb4e14
talhaceliktas <avrol928@gmail.com>
2026-04-02 18:30:06 +0300

    feat: implement strongly-typed options for WeatherAPI configuration
    Introduces a WeatherApiOptions class to bind the "WeatherAPI" configuration section, replacing manual string-based lookups in the HomeController with a structured object approach.

--- ConfigurationAndHttpClient/ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationAndHttpClient/ConfigurationExample/Controllers/HomeController.cs
@@ -14,8 +14,23 @@ namespace ConfigurationExample.Controllers
         [HttpGet("/")]
         public IActionResult Index()
         {
-            ViewBag.ClientID = _configuration.GetValue<string>("WeatherAPI:ClientID", "Default ID");
-            ViewBag.ClientSecret = _configuration.GetValue<string>("WeatherAPI:ClientSecret", "Default Secret");
+            //ViewBag.ClientID = _configuration.GetValue<string>("WeatherAPI:ClientID", "Default ID");
+            //ViewBag.ClientSecret = _configuration.GetValue<string>("WeatherAPI:ClientSecret", "Default Secret");
+
+            //ViewBag.ClientID = _configuration.GetSection("WeatherAPI").GetValue<string>("ClientID", "Default ID");
+            //ViewBag.ClientSecret = _configuration.GetSection("WeatherAPI").GetValue<string>("ClientSecret", "Default Secret");
+
+            //IConfigurationSection weatherApiSection = _configuration.GetSection("WeatherAPI");
+
+            //ViewBag.ClientID = weatherApiSection["ClientID"];
+            //ViewBag.ClientSecret = weatherApiSection["ClientSecret"];
+
+            WeatherApiOptions? weatherApiSection = _configuration.GetSection("WeatherAPI")
+                .Get<WeatherApiOptions>();
+
+
+            ViewBag.ClientID = weatherApiSection?.ClientID ?? "Default Client ID";
+            ViewBag.ClientSecret = weatherApiSection?.ClientSecret ?? "Default Secret";
 
             return View();
         }

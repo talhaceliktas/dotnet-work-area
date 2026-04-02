f16e236dea6421dfe5eb7f3ce600cfb303544d8f
talhaceliktas <avrol928@gmail.com>
2026-04-02 12:33:18 +0300

    feat: add OperationController and enable routing in LifetimeDemo
    Implements the OperationController to consume the registered transient, scoped, and singleton services. Updates the application configuration to support controllers and mapping, enabling the demonstration of service lifetimes through web requests.

---
+++ DependencyInjection/Examples/Task-2/LifetimeDemo/Controllers/OperationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+
+namespace LifetimeDemo.Controllers
+{
+    public class OperationController : Controller
+    {
+        private readonly ITransientOperation _transientOperation;
+        private readonly IScopedOperation _scopedOperation;
+        private readonly ISingletonOperation _singletonOperation;
+
+        public OperationController(
+            ITransientOperation transientOperation,
+            IScopedOperation scopedOperation,
+            ISingletonOperation singletonOperation
+        ) {
+            _transientOperation = transientOperation;
+            _scopedOperation = scopedOperation;
+            _singletonOperation = singletonOperation;
+        
+        }
+
+    }
+}
--- DependencyInjection/Examples/Task-2/LifetimeDemo/Program.cs
+++ DependencyInjection/Examples/Task-2/LifetimeDemo/Program.cs
@@ -3,11 +3,14 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddTransient<ITransientOperation, TransientOperation>();
 builder.Services.AddScoped<IScopedOperation, ScopedOperation>();
 builder.Services.AddSingleton<ISingletonOperation, SingletonOperation>();
 
 var app = builder.Build();
 
+app.MapControllers();
 
 app.Run();

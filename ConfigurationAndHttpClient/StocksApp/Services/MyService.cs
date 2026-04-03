63d9f6db9aca47256e9ab6f9e70957bffd4fb3bb
talhaceliktas <avrol928@gmail.com>
2026-04-03 12:06:12 +0300

    feat: register HttpClient services and implement service using IHttpClientFactory
    Registers the HttpClient services in the dependency injection container and adds a new service class that demonstrates how to use IHttpClientFactory to create clients and send asynchronous requests.

---
+++ ConfigurationAndHttpClient/StocksApp/Services/MyService.cs
@@ -0,0 +1,27 @@
+﻿namespace StocksApp.Services
+{
+    public class MyService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MyService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task method()
+        {
+            using (HttpClient httpClient = _httpClientFactory.CreateClient()) {
+                HttpRequestMessage requestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri("url"),
+                    Method = HttpMethod.Get,
+                };
+
+                HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+
+            
+            }
+        }
+    }
+}

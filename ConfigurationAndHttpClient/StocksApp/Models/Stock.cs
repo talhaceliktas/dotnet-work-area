8300a9d3c8f5016aabf887016dfe16537062b89d
talhaceliktas <avrol928@gmail.com>
2026-04-03 13:09:08 +0300

    feat: introduce Stock model to represent quote data
    Adds a new Stock class in the Models namespace to store stock-related information, including current price, daily high/low, and price changes. This provides a structured model for handling stock data within the application.

---
+++ ConfigurationAndHttpClient/StocksApp/Models/Stock.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Threading.Channels;
+
+namespace StocksApp.Models
+{
+    public class Stock
+    {
+        public string? StockSymbol { get; set; }
+
+        public double CurrentPrice { get; set; }
+
+        public double Change {  get; set; }
+
+        public double PercentChange { get; set; }
+
+        public double HighPriceOfTheDay { get; set; }
+
+        public double LowPriceOfTheDay { get; set; }
+
+        public double OpenPriceOfTheDay { get; set; }
+
+        public double PreviousClosePrice { get; set; }
+    }
+}

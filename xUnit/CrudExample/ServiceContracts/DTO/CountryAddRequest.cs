83dcc140ca96d3e9985c612f1bec41c594600e5e
talhaceliktas <avrol928@gmail.com>
2026-04-04 11:54:33 +0300

    feat: implement CountryAddRequest DTO
    Defines the data transfer object for adding new country records, including a mapping method to convert the DTO into a Country domain model. This establishes the necessary contract for the service layer to handle country creation.

---
+++ xUnit/CrudExample/ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class for adding a new country
+    /// </summary>
+    public record CountryAddRequest
+    {
+        public string? CountryName { get; init; }
+
+        public Country ToCountry()
+        {
+            return new Country() { CountryName = CountryName };
+        }
+    }
+}

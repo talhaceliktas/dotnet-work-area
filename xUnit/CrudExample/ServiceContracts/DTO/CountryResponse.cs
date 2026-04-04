f9bd7f01559648dc97f4a8e1e206b03477c3e590
talhaceliktas <avrol928@gmail.com>
2026-04-04 12:01:48 +0300

    feat: implement CountryResponse DTO
    Defines the data transfer object for returning country information from service methods. Includes an extension method to map the Country domain model to the DTO, ensuring a clean separation between domain entities and the service layer's output.

---
+++ xUnit/CrudExample/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class that is used as return type for most of CountriesService methods
+    /// </summary>
+    public record CountryResponse
+    {
+        public Guid CountryId { get; init; }
+
+        public string? CountryName { get; init; }
+    }
+
+    public static class CountryExtensions
+    {
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse() 
+            { CountryId = country.CountryID, CountryName = country.CountryName };
+        }
+    }
+}
